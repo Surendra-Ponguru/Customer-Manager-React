0025cf105270dad04daa665b6b2829c93c16d75c
Surendra-Ponguru <surendra.ponguru@kanerika.com>
2022-10-14 18:46:36 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ import CustomerParams from "./Component/CustomerDetail";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter  basename="/Customer-Manager-React">
     {/* <Switch>
           <PublicRoute restricted={false} component={<Login/>} path="/" exact />
           <PublicRoute restricted={true} component={onLogin} path="/" exact />
